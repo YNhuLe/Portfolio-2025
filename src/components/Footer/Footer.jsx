2a94dd49acdb48eaec816371513bea4a27472af5
NhuYLe <lenhuy10011996@gmail.com>
2025-04-22 14:56:10 -0400

    add more margin to the project title and clean up the code base

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,15 +1,32 @@
 import "./Footer.scss";
 import githubIcon from "/logos/git3D.png";
 import linked from "/logos/linked.png";
+import Tilt from "react-parallax-tilt";
 function Footer() {
   return (
     <div className="footer">
-      <a href="">
-        <img src={githubIcon} alt="github" className="footer_icon" />
-      </a>
-      <a href="">
-        <img src={linked} alt="linked" className="footer_icon" />
-      </a>
+      <Tilt
+        glareEnable={true}
+        glareMaxOpacity={0.2}
+        scale={1.05}
+        transitionSpeed={250}
+        tiltMaxAngleX={15}
+        tiltMaxAngleY={15}>
+        <a href="https://github.com/YNhuLe">
+          <img src={githubIcon} alt="github" className="footer_icon" />
+        </a>
+      </Tilt>
+      <Tilt
+        glareEnable={true}
+        glareMaxOpacity={0.2}
+        scale={1.05}
+        transitionSpeed={250}
+        tiltMaxAngleX={15}
+        tiltMaxAngleY={15}>
+        <a href="https://www.linkedin.com/in/jennynhuyle/">
+          <img src={linked} alt="linked" className="footer_icon" />
+        </a>
+      </Tilt>
     </div>
   );
 }
