464d976692373bf8eba9b82c9403d177a122f098
NhuYLe <lenhuy10011996@gmail.com>
2025-06-09 23:12:42 -0400

    change the style of the project card

--- src/components/MasonryLayout/MasonryLayout.jsx
+++ src/components/MasonryLayout/MasonryLayout.jsx
@@ -1,45 +1,62 @@
+import Tilt from "react-parallax-tilt";
 import "./MasonryLayout.scss";
-import { FaGithub, FaGlobe } from "react-icons/fa";
+import { FaGithub, FaGlobe, FaExternalLinkAlt } from "react-icons/fa";
 function MasonryLayout({ items }) {
   return (
     <div className="masonry__layout">
       {items.map((item) => (
         // <div className="card__border" style={{ width: `${item.width}rem` }}>
-        <article
-          key={item.id}
-          className="masonry__item"
-          // style={{ width: `${item.width}rem` }}
-        >
-          <div className="img__project">
-            <img
-              src={item.img_url}
-              alt="project-image"
-              className="project__img"
-              loading="lazy"
-            />
-          </div>
-          <h2 className="project__title">{item.title}</h2>
-          <div className="project__concept">
-            <p className="project__text"> {item.concept}</p>
-            <div className="link">
+        <Tilt
+          glareEnable={true}
+          glareMaxOpacity={0.3}
+          scale={1.05}
+          transitionSpeed={300}
+          tiltMaxAngleX={15}
+          tiltMaxAngleY={15}
+          key={item.id}>
+          <article className="masonry__item">
+            <div className="img__project">
+              <a href="/project-details" className="arrow-icon">
+                <FaExternalLinkAlt size={20} />
+              </a>
+              {/* <div className="link">
               <a href={item.gitUrl} className="project__link">
                 <FaGithub className="github-icon" size={24} />
               </a>
               <a href={item.link} className="project__link">
                 <FaGlobe className="globe-icon" size={24} />
               </a>
+            </div> */}
+              <img
+                src={item.img_url}
+                alt="project-image"
+                className="project__img"
+                loading="lazy"
+              />
             </div>
-          </div>
-          <div className="stack">
-            <ul className="stack__list">
-              {item.stack.map((tech, index) => (
-                <li className="stack__item" key={index}>
-                  #{tech}
-                </li>
-              ))}
-            </ul>
-          </div>
-        </article>
+            <h2 className="project__title">{item.title}</h2>
+            <div className="project__concept">
+              <p className="project__text"> {item.concept}</p>
+              {/* <div className="link">
+              <a href={item.gitUrl} className="project__link">
+                <FaGithub className="github-icon" size={24} />
+              </a>
+              <a href={item.link} className="project__link">
+                <FaGlobe className="globe-icon" size={24} />
+              </a>
+            </div> */}
+            </div>
+            <div className="stack">
+              <ul className="stack__list">
+                {item.stack.map((tech, index) => (
+                  <li className="stack__item" key={index}>
+                    #{tech}
+                  </li>
+                ))}
+              </ul>
+            </div>
+          </article>
+        </Tilt>
         // </div>
       ))}
     </div>
